4960284ce6b4f8c3ca37c16d17877c2ffdadd819
Abhijeet mishra <99529447+abhijeetnavodayan@users.noreply.github.com>
2022-06-02 14:19:56 +0530

    Delete test.py

--- file handling/test.py
+++
@@ -1,7 +0,0 @@
-try:
-foo = open ( 'test.txt', 'w' )
-foo.write ( "It's a test file to verify try-finally in exception handling!!")            
-print 'try block executed'
-finally:
-foo.close ()
-print 'finally block executed'
